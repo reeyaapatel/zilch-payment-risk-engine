0bfff601d86ccad506bce8c3b25eb642bbf521ce
Reeya Patel <reeya@Reeyas-MBP.communityfibre.co.uk>
2026-05-27 18:57:43 +0100

    Initial commit

---
+++ src/main/java/com/reeya/payment_risk_engine/PaymentRiskEngineApplication.java
@@ -0,0 +1,13 @@
+package com.reeya.payment_risk_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRiskEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRiskEngineApplication.class, args);
+	}
+
+}
